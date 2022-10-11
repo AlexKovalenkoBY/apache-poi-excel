3f891149abe720cbd0918f3eac11bb7c8fbf96e4
AlexKovalenkoBY <kovalenkoam@gmail.com>
2022-10-11 12:28:28 +0300

    сделал вычитывание файла

--- java-read-large-excel-file-apache-poi/src/main/java/com/roytuts/java/read/large/excel/file/apache/poi/LargeExcelReaderApp.java
+++ java-read-large-excel-file-apache-poi/src/main/java/com/roytuts/java/read/large/excel/file/apache/poi/LargeExcelReaderApp.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 
 import org.apache.poi.EncryptedDocumentException;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.ss.usermodel.DateUtil;
@@ -15,29 +16,48 @@ import org.apache.poi.xssf.usermodel.XSSFSheet;
 public class LargeExcelReaderApp {
 
 	public static void main(String[] args) throws Exception {
-		String fileName = "Sales-Records.xlsx";
+		String fileName = "ARMTEK_MAIN_40006905_202210041806.xlsx";
 		// readLargeExcelFile(fileName);
-
+		System.out.println("started");
+		
 		SaxEventUserModel saxEventUserModel = new SaxEventUserModel();
+		
+        long startTime = System.nanoTime();
 		saxEventUserModel.processSheets(fileName);
+		long estimatedTime = System.nanoTime() - startTime;
+       
+        System.out.println("estimatedTime: " + estimatedTime / 1_000_000_000.);
+		System.out.println("started V2");
+		startTime = System.nanoTime();
+		readLargeExcelFile(fileName);
+		estimatedTime = System.nanoTime() - startTime;
+       
+        System.out.println("estimatedTime: " + estimatedTime / 1_000_000_000.);
 	}
 
 	// The following method will give error - OutOfMemoryError
-	public static void readLargeExcelFile(final String fileName) throws EncryptedDocumentException, IOException {
+	public static void readLargeExcelFile(final String fileName)
+			throws EncryptedDocumentException, IOException, InvalidFormatException {
 		Workbook wb = WorkbookFactory.create(new File(fileName));
 
 		XSSFSheet sheet = (XSSFSheet) wb.getSheetAt(0);
-
+		int rowNum = 0;
 		for (Row r : sheet) {
-			for (Cell c : r) {
-				CellType cellType = c.getCellType();
-				if (CellType.STRING.equals(cellType)) {
-					System.out.println(c.getStringCellValue());
-				} else if (CellType.NUMERIC.equals(cellType)) {
-					System.out.println(String.valueOf(c.getNumericCellValue()));
-				} else if (DateUtil.isCellDateFormatted(c)) {
-					System.out.println(c.getDateCellValue());
+			rowNum++;
+			try {
+
+				for (Cell c : r) {
+					CellType cellType = c.getCellType();
+					if (CellType.STRING.equals(cellType)) {
+						// System.out.println(c.getStringCellValue());
+					} else if (CellType.NUMERIC.equals(cellType)) {
+						// System.out.println(String.valueOf(c.getNumericCellValue()));
+					} else if (DateUtil.isCellDateFormatted(c)) {
+						// System.out.println(c.getDateCellValue());
+					}
 				}
+			} catch (Exception e) {
+				System.out.println(rowNum);
 			}
 		}
 	}
--- java-read-large-excel-file-apache-poi/src/main/java/com/roytuts/java/read/large/excel/file/apache/poi/SaxEventUserModel.java
+++ java-read-large-excel-file-apache-poi/src/main/java/com/roytuts/java/read/large/excel/file/apache/poi/SaxEventUserModel.java
@@ -21,7 +21,7 @@ public class SaxEventUserModel {
 	public void processSheets(String filename) throws Exception {
 		OPCPackage pkg = OPCPackage.open(filename);
 		XSSFReader r = new XSSFReader(pkg);
-		SharedStringsTable sst = r.getSharedStringsTable();
+		SharedStringsTable sst = (SharedStringsTable) r.getSharedStringsTable();
 		XMLReader parser = fetchSheetParser(sst);
 		Iterator<InputStream> sheets = r.getSheetsData();
 		while (sheets.hasNext()) {
@@ -30,8 +30,9 @@ public class SaxEventUserModel {
 			InputSource sheetSource = new InputSource(sheet);
 			parser.parse(sheetSource);
 			sheet.close();
-			System.out.println("");
+			System.out.println("sheet proceed");
 		}
+		pkg.close();
 	}
 
 	public XMLReader fetchSheetParser(SharedStringsTable sst) throws SAXException, ParserConfigurationException {
@@ -58,7 +59,7 @@ public class SaxEventUserModel {
 			// c => cell
 			if (name.equals("c")) {
 				// Print the cell reference
-				System.out.print(attributes.getValue("r") + " - ");
+				//System.out.print(attributes.getValue("r") + " - ");
 				// Figure out if the value is an index in the SST
 				String cellType = attributes.getValue("t");
 				if (cellType != null && cellType.equals("s")) {
@@ -76,14 +77,14 @@ public class SaxEventUserModel {
 			// Process the last contents as required.
 			// Do now, as characters() may be called more than once
 			if (nextIsString) {
-				int idx = Integer.parseInt(lastContents);
-				lastContents = sst.getItemAt(idx).getString();
+				int idx = Integer.parseInt(lastContents.trim());
+				lastContents = sst.getItemAt(idx).getString().trim();
 				nextIsString = false;
 			}
 			// v => contents of a cell
 			// Output after we've seen the string contents
 			if (name.equals("v")) {
-				System.out.println(lastContents);
+				// System.out.println(lastContents);
 			}
 		}
 
